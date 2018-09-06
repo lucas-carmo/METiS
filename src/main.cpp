211dbfae5e58d7d69150d9d683613346779993a8
Lucas Carmo <lucas.carmo@usp.br>
2018-09-06 16:16:11 -0300

    Removed TODO list in main.cpp (using Glo Boards now)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-/* To do: 
-   No IO.cpp, adicionar um warning pra caso a keyword seja desconhecida. Provavelmente fica melhor trocar aqueles ifs por cases
-
-   Depois que criar todas as funcoes de leitura separadamente, ver oq da pra transformar em uma funcao comum a elas.
-   Ex: Uma funcao checkInput pra verificar se tem o numero certo de elementos pra leitura, se as conversoes pros tipos numericos
-   do input deram certo, coisa do tipo. Assim facilita a manutencao, pq se quiser mudar mensagem de erro ou coisa do tipo, mudo
-   em um so lugar   
-*/
-
 #include <iostream>
 #include <chrono> // For std::chrono functions. It is useful to time the code and verify whether one method or another will be more performant
 #include <armadillo> // Linear algebra library with usage similar to MATLAB
