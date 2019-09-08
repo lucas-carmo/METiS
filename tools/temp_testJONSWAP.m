ec13fdfc8d29729ea93ed4caa6bc153d166eb007
Lucas <lucas.carmo@usp.br>
2019-09-08 11:31:00 -0300

    update JONSWAP script

--- tools/temp_testJONSWAP.m
+++ tools/temp_testJONSWAP.m
@@ -5,20 +5,29 @@ close all
 clc
 
 % Simulation parameters
-Tmax = 3600;
+Tmax = 10800;
 dt = 0.25;
 t = 0:dt:Tmax;
 
 
+
 % JONSWAP parameters
 Hs = 2;
-Tp = 11;
+Tp = 20;
 gamma = 3.3;
 dir = 45;
-wlow = 0.314159;
-whigh = 1.570596;
+wlow = 0.18;
+whigh = 0.92;
 
+% Plot characteristics
+width4Line = 2;
+size4Font = 25;
+weight4Font = 'bold';
+aspRatio = [3 1 1];
+color4Line = [0, 0.4470, 0.7410];
+size4Marker = 10;
 
+addpath('C:\Users\lucas.henrique\Google Drive\Doutorado\1Casos_teste\export-fig')
 
 %========================================================================
 dw = 2*pi/Tmax;
@@ -35,8 +44,8 @@ A = exp( -0.5 * ((w/wp-1)./sigma).^2 );
 
 Sw = 0.3125 * Hs^2 * Tp * (w/wp).^(-5) .* exp(-1.25 * (wp./w).^4 ) .* (1-0.287*log(gamma)) .* gamma.^A;
 
-figure('color', 'w')
-plot(w,Sw)
+% figure('color', 'w')
+% plot(w,Sw)
 
 
 % Calcula as amplitudes
@@ -67,25 +76,44 @@ for ii = 1:length(amp)
     y = y + amp(ii)*cos(omega(ii)*t + phase(ii));
 end
 
-figure('color', 'w')
-plot(t, y)
 
+%===
+%=== PLOTA FIGURA NO DOMINIO DO TEMPO
+%===
+outFig = figure('color', 'w', 'units','normalized','outerposition',[0 0 1 1]);
+plot(t, y, 'lineWidth', width4Line)
+xlim([0 t(end)])
+xlabel('Time (s)')
+ylabel('First-order wave elevation (m)')
+set(gca, 'fontSize', size4Font);
+% export_fig(outFig, 'ilust_JONSWAP_timeSeries.pdf');
+
+
+%===
+%=== PLOTA ESPECTRO
+%===
 
 % Testar se bate com o espectro de entrada
 [Sres, w2] = pwelch(y,[],[],[],1/dt);  
 Sres = Sres/(2*pi); % passar o espectro para rad/s 
 w2 = 2*pi*w2;
 
-figure('color', 'w')
-plot(w2,Sres, 'b')
-hold on
-plot(w,Sw, 'r')
-legend('From time series', 'JONSWAP')
-xlim([0 2*whigh])
 
+outFig = figure('color', 'w', 'units','normalized','outerposition',[0 0 1 1]);
+
+
+plot(w2,Sres, 'b', 'lineWidth', width4Line)
 hold on
-plot([wlow wlow], [0 max(1.25*Sw)], '--k')
+plot(w,Sw, 'r', 'lineWidth', width4Line)
 hold on
-plot([whigh whigh], [0 max(1.25*Sw)], '--k')
+plot([wlow whigh], [0 0], 'o', 'markerSize', size4Marker, 'Color', 'k', 'MarkerFace', 'k')  
+xlim([0 2*whigh])
+xlabel('Frequency (rad/s)')
+ylabel('Spectral density (m^2.s)')
+
+legend('From time series', 'JONSWAP')
+set(gca, 'fontSize', size4Font);
+
+% export_fig(outFig, 'ilust_JONSWAP_espectro.pdf');
 
 
